00e19116b169e8a56d412751b20ea1562642b221
victor30381 <victor30381@github.com>
2026-03-31 07:14:48 -0300

    Ocultar llaves privadas de Firebase

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8V-IaUWw-5M44lsBpBedHM22LN6CknkM",
-  authDomain: "gestion-de-comercio-483a3.firebaseapp.com",
-  projectId: "gestion-de-comercio-483a3",
-  storageBucket: "gestion-de-comercio-483a3.firebasestorage.app",
-  messagingSenderId: "1083154392089",
-  appId: "1:1083154392089:web:99d224c23ca3b9b9ce3688",
-  measurementId: "G-EXZD03V8WV"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
